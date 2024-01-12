3aeb8b8f550b0e64c44690f75235962224128ed9
Eshwanth J <eshwanthj@gmail.com>
2024-01-12 12:23:47 +0000

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import SearchInput from "./SearchInput";
+import WeatherContent from "./WeatherContent";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="weather-card">
+        <SearchInput />
+        <WeatherContent />
+      </div>
+    </div>
+  );
+}
+
+export default App;
